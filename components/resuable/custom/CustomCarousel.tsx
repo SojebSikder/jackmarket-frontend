0b6f9dc35ad3c18c32493f38f082daa85788b1e2
sojebsikder <sojebsikder@gmail.com>
2023-01-14 22:42:18 +0600

    updated carousel component

--- components/resuable/custom/CustomCarousel.tsx
+++ components/resuable/custom/CustomCarousel.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import CustomImage from "./CustomImage";
+import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
 
 export default function CustomCarousel({ images }: { images: any[] }) {
   return (
-    <Carousel>
+    <Carousel
+      prevIcon={<BsChevronLeft style={{ color: "black" }} />}
+      nextIcon={<BsChevronRight style={{ color: "black" }} />}
+    >
       {images.map((image) => {
         return (
           <Carousel.Item key={image.id}>
             <CustomImage
+              style={{ objectFit: "contain" }}
               className="d-block w-100"
               src={image.image_url}
               alt={`${image.alt_text}`}
