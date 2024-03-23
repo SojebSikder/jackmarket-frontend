d078823a8005c1678d1f26c73a308ab2ecbcc7b0
sojebsikder <sojebsikder@gmail.com>
2024-03-23 17:37:15 +0600

    initial commit

--- components/partial/Main.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Main({ children }: { children: any }) {
-  return <main className="h-100">{children}</main>;
-}
--- components/resuable/layout/Container.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Container as BsContainer } from "react-bootstrap";
-
-export default function Container({ children }: { children: any }) {
-  return <BsContainer>{children}</BsContainer>;
-}
